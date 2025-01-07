40bcf06aaa16df3ab6ea474cc10dab23f594a435
Samuels7 <66149372+samuels97@users.noreply.github.com>
2025-01-07 18:40:56 +0300

    Update regular_exp.py

--- Regexes/regular_exp.py
+++ Regexes/regular_exp.py
@@ -9,3 +9,7 @@ pattern = r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})"
 date_string = "2024-07-25"
 match = re.match(pattern, date_string)
 print("Year: ", match.group("year"))
+
+xmasRegex = re.compile(r'\d+\s\w+')
+xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans')
+
